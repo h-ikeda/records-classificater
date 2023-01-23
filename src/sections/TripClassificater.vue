c4bfbefee1f75d76ae4d2aa864bb51e203b9d305
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2023-01-24 01:31:47 +0900

    スタイルを適用 (#6)
    * Bumps firebase from 9.15.0 to 9.16.0.
    
    * スタイルを適用
    
    * parcel設定のリライト
    
    * viewportの設定
    
    * ページ全体を左右中央配置

--- src/sections/TripClassificater.vue
+++ src/sections/TripClassificater.vue
@@ -1,18 +1,33 @@
 <template>
-  <h2>Trip classificater</h2>
-  <ul>
-    <li v-for="trip in trips">
-      <dl>
-        <dt>Time</dt>
-        <dd>{{ trip.timestamp.toDate().toString() }}</dd>
-        <dt>ODO</dt>
-        <dd>{{ trip.odo }}</dd>
-        <dt>Class</dt>
-        <dd>{{ trip.class }}</dd>
+  <h2 class="py-0.5 px-4 text-sm bg-gray-300 -mx-4 sticky top-0 font-black">Trip classificater</h2>
+  <section class="my-2 border-y-4 py-1 border-gray-300">
+    <h3 class="font-black text-sm text-center mb-1">Summary</h3>
+    <dl class="grid grid-flow-row w-fit gap-x-2 mx-auto auto-cols-fr">
+      <template v-for="(value, key) in classSummaries" :key="key">
+        <dt class="row-start-1 text-xs bg-gray-300 px-2 text-center font-medium">{{ key }}</dt>
+        <dd class="row-start-2 text-end border-b px-1">{{ value }} km</dd>
+        <dd class="text-sm text-end px-1" v-if="sum">{{ Math.round(value / sum * 1000) / 10 }} %</dd>
+      </template>
+    </dl>
+  </section>
+  <ul class="w-fit mx-auto flex flex-col-reverse">
+    <li v-for="{timestamp, odo, trip, class: cls} in calculatedTrips" class="border-b-2 border-gray-500 my-4">
+      <dl class="grid items-center gap-x-2">
+        <dt class="after:content-[':'] text-sm">記録日時</dt>
+        <dd class="col-start-2 border-b border-dotted border-black my-1">{{ formatDate(timestamp) }}</dd>
+        <dt class="after:content-[':'] text-sm">総走行距離 (ODO)</dt>
+        <dd class="text-lg text-end border-b border-dotted border-black">{{ odo }} km</dd>
+        <template v-if="trip !== undefined">
+          <dt class="after:content-[':'] text-sm">走行距離 (TRIP)</dt>
+          <dd class="text-lg text-end border-b border-dotted border-black">{{ trip }} km</dd>
+        </template>
+        <dt class="after:content-[':'] text-sm">分類</dt>
+        <dd class="text-sm my-1">{{ cls }}</dd>
       </dl>
     </li>
-    <li v-if="currentUser">
-      <NewTrip :min-odo="lastODO" @submit="createTrip" />
+    <li class="w-fit mx-auto py-4" v-if="currentUser">
+      <button v-if="!newTripEnabled" @click="newTripEnabled = true" class="border-2 rounded-full w-10 h-10 border-gray-500 text-gray-500 text-xl font-bold flex items-center justify-center">+</button>
+      <NewTrip v-if="newTripEnabled" class="border-b py-4 border-gray-500" :min-odo="lastODO" @submit="createTrip" @cancel="newTripEnabled = false" />
     </li>
   </ul>
 </template>
@@ -29,6 +44,20 @@ const { currentUser } = toRefs(props);
 
 const db = getFirestore();
 const trips = ref([]);
+const newTripEnabled = ref(false);
+const calculatedTrips = computed(() => trips.value.reduce((acc, trip) => {
+  acc.push(acc.length ? { ...trip, trip: trip.odo - acc.at(-1).odo } : trip);
+  return acc;
+}, []));
+const classSummaries = computed(() => calculatedTrips.value.reduce((acc, { class: c, trip }) => {
+  if (!(c in acc)) acc[c] = 0;
+  if (trip !== undefined) acc[c] += trip;
+  return acc;
+}, {}));
+const sum = computed(() => {
+  if (trips.value.length < 2) return 0;
+  return trips.value.at(-1).odo - trips.value[0].odo;
+});
 
 let unsub;
 watch(currentUser, (user, oldUser, invalidate) => {
@@ -38,7 +67,11 @@ watch(currentUser, (user, oldUser, invalidate) => {
       trips.value = snapshot.data().data;
     });
     invalidate(unsub);
+  } else {
+    trips.value = [];
   }
+}, {
+  immediate: true,
 });
 
 const lastODO = computed(() => {
@@ -46,6 +79,11 @@ const lastODO = computed(() => {
   if (!last) return 0;
   return last.odo;
 });
+
+function formatDate(timestamp) {
+  const d = timestamp.toDate();
+  return `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日 ${d.getHours().toString().padStart(2, '0')}:${d.getMinutes().toString().padStart(2,'0')}`;
+}
 
 function sortByTimestamp({ timestamp: t1 }, { timestamp: t2 }) {
   return t1 - t2;
@@ -55,5 +93,6 @@ function createTrip(trip) {
   setDoc(doc(db, 'trips', currentUser.value.uid), {
     data: [...trips.value, trip].sort(sortByTimestamp),
   });
+  newTripEnabled.value = false;
 }
 </script>
