8d97c962948b9116feb5296bbed2cb421b58183d
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2023-02-05 05:31:21 +0000

    auto signin as test user on preview & local env

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,13 +1,13 @@
 <template>
   <SignOut v-if="currentUser" />
-  <SignInWithPassword v-else-if="dev" />
+  <SignInWithTestEmailPassword v-else-if="dev" />
   <SignInWithGoogle v-else />
 </template>
 
 <script setup>
 import { toRefs } from 'vue';
 import SignInWithGoogle from './SignInWithGoogle.vue';
-import SignInWithPassword from './SignInWithPassword.vue';
+import SignInWithTestEmailPassword from './SignInWithTestEmailPassword.vue';
 import SignOut from './SignOut.vue';
 
 const dev = process.env.NODE_ENV !== 'production';
