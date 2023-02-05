c510750d4f6981931faafb8319eb813baa3cb396
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2023-02-05 05:46:31 +0000

    not to set emulators on preview env

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 export default process.env.PROJECT_ID !== 'records-classificater' ? {
   apiKey: "AIzaSyCdEu_wJyhSKJZgIDSZOZPP3NIor-wZrSQ",
-  authDomain: location.hostname,
+  authDomain: "records-classificater-test.web.app",
   projectId: "records-classificater-test",
   storageBucket: "records-classificater-test.appspot.com",
   messagingSenderId: "621218742775",
