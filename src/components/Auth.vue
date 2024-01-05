e96e50d71a277bab2abff4a8e26d8e0ed99ce185
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2024-01-05 13:22:57 +0000

    seeding mock data for local dev env

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,7 +1,10 @@
 <template>
-  <SignOut v-if="currentUser" />
-  <SignInWithTestEmailPassword v-else-if="dev" />
-  <SignInWithGoogle v-else />
+  <nav class="grid grid-col gap-2">
+    <SignOut v-if="currentUser" />
+    <SignInWithTestEmailPassword v-else-if="dev" />
+    <SignInWithGoogle v-else />
+    <DeleteAccount v-if="local && currentUser" :current-user="currentUser"/>
+  </nav>
 </template>
 
 <script setup>
@@ -9,8 +12,10 @@ import { toRefs } from 'vue';
 import SignInWithGoogle from './SignInWithGoogle.vue';
 import SignInWithTestEmailPassword from './SignInWithTestEmailPassword.vue';
 import SignOut from './SignOut.vue';
+import DeleteAccount from './DeleteAccount.vue';
 
 const dev = process.env.PROJECT_ID !== 'records-classificater';
+const local = process.env.NODE_ENV !== 'production';
 
 const props = defineProps({
   currentUser: { default: null },
