0fdbda005baf20c1219e86751ff1862d835873f2
IKEDA Hiroki <ikeda_hiroki@icloud.com>
2024-01-10 13:33:44 +0000

    認証状態確認中にローディング画面を表示する

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,43 @@
 <template>
-  <TripClassificater :currentUser="currentUser" />
-  <Suspense>
-    <Auth :currentUser="currentUser" class="w-fit mx-auto block text-red-600 font-medium text-sm" />
-  </Suspense>
+  <Loader v-if="currentUser === undefined" class="loader" />
+  <main v-else>
+    <nav>
+      <h2 class="font-medium">Trip classificater</h2>
+      <Auth :current-user="currentUser" />
+    </nav>
+    <TripClassificater v-if="currentUser" :currentUser="currentUser" />
+  </main>
 </template>
 
-<script setup>
+<script setup lang="ts">
+import type { User } from 'firebase/auth';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
-import { ref } from 'vue';
+import type { Ref } from 'vue';
+import { onBeforeUnmount, ref } from 'vue';
 import Auth from './components/Auth.vue';
 import TripClassificater from './sections/TripClassificater.vue';
+import Loader from './components/Loader.vue';
 
-const currentUser = ref(null);
+const currentUser: Ref<User|null|undefined> = ref(undefined);
 const auth = getAuth();
-onAuthStateChanged(auth, user => {
-  if ((user && user.uid) === (currentUser.value && currentUser.value.uid)) return;
-  currentUser.value = user;
-});
+
+onBeforeUnmount(
+  onAuthStateChanged(auth, user => {
+    currentUser.value = user;
+  },
+));
 </script>
+
+<style scoped lang="scss">
+.loader {
+  @apply fixed inset-0 bg-slate-100 text-green-300 text-5xl;
+}
+
+main > nav {
+  @apply px-2 bg-lime-200 sticky top-0 flex;
+
+  h2:first-child {
+    @apply grow;
+  }
+}
+</style>
